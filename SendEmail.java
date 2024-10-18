acf74e0368e31e56f0dc8b56522cfd7452306bc6
Prince Obiuto <152011741+Prince-Obiuto@users.noreply.github.com>
2024-10-18 20:48:15 +0100

    Update

---
+++ SendEmail.java
@@ -0,0 +1,47 @@
+import java.util.Properties;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class SendEmail {
+
+private Properties loadProperties() throws IOException {
+        Properties property = new Properties();
+        FileInputStream fis = new FileInputStream("config.properties");
+        props.load(fis);
+        return props;
+    }
+
+    public static void createEmail(String recipientEmail, String firstName, String lastName) {
+        try {
+            Properties props = loadProperties();
+            final String username = props.getProperty("email.username");
+            final String password = props.getProperty("email.password");
+
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail));
+            message.setSubject("Registration Confirmation");
+            message.setText(String.format("Dear %s %s,\n\nThank you for registering!", firstName, lastName));
+
+            Transport.send(message);
+            System.out.println("Email sent successfully to " + recipientEmail);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}

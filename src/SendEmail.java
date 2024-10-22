2b745b806666834043a4ac4046e3b40143b0f1e0
Prince Obiuto <152011741+Prince-Obiuto@users.noreply.github.com>
2024-10-22 09:18:20 +0100

    Update SendEmail.java

--- src/SendEmail.java
+++ src/SendEmail.java
@@ -23,11 +23,10 @@ public class SendEmail {
         final String username = props.getProperty("email.username");
         final String password = props.getProperty("email.password");
 
-        Properties properties = new Properties();
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp-relay.brevo.com");
-        properties.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp-relay.brevo.com");
+        props.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
